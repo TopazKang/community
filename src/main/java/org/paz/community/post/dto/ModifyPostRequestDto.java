5a15db1548e230bec300d5f6d9b91f3f8bc008da
TopazKang <lasksla@gmail.com>
2024-10-25 14:58:46 +0900

    feat: JPA 기반 post/comment 기능 추가
    Post 서비스 interface/구현체 추가
    Post 수정 Req Dto 추가
    Post/Comment 엔티티의 연관관계를 위한 MemberJpaRepository 추가

---
+++ src/main/java/org/paz/community/post/dto/ModifyPostRequestDto.java
@@ -0,0 +1,12 @@
+package org.paz.community.post.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+public class ModifyPostRequestDto {
+    private String title;
+    private String content;
+}
---
+++ src/main/java/org/paz/community/post/service/PostService.java
@@ -0,0 +1,22 @@
+package org.paz.community.post.service;
+
+import org.paz.community.post.dto.ModifyPostRequestDto;
+import org.springframework.web.multipart.MultipartFile;
+import org.paz.community.post.dto.CreatePostRequestDto;
+import org.paz.community.post.dto.ReadOnePostResponseDto;
+import org.paz.community.post.dto.ReadSummaryPostResponseDto;
+
+import java.util.List;
+
+public interface PostService {
+    // 게시글 작성
+    void createPost(String token, CreatePostRequestDto data, List<MultipartFile> files);
+    // 전체 게시글 조회
+    List<ReadSummaryPostResponseDto> readAllPost();
+    // 단일 게시글 조회
+    ReadOnePostResponseDto readOnePost(Long postId);
+    // 게시글 수정
+    void modifyPost(Long postId, ModifyPostRequestDto data, List<MultipartFile> files);
+    // 게시글 삭제
+    void deletePost(Long postId);
+}
