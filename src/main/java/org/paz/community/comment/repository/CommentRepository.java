cd4de93e7eedeab7fd254642e05d8644d1598eb0
TopazKang <lasksla@gmail.com>
2024-10-25 15:04:58 +0900

    fix: CommentRepository 커스텀 interface 추가
    Post 서비스에서 단일 게시글 조회시, 게시글에 연관된 모든 댓글을 받아올 수 있게끔 커스텀 interface 추가

--- src/main/java/org/paz/community/comment/repository/CommentRepository.java
+++ src/main/java/org/paz/community/comment/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package org.paz.community.comment.repository;
 import org.paz.community.comment.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByPostId(Long postId);
 }
