ead46584a7c354570383fe70a5fcb991d032e69b
TopazKang <lasksla@gmail.com>
2024-10-25 16:16:33 +0900

    fix: Member 컨트롤러 상위 엔드포인트 수정
    Member 컨트롤러 상위 엔드포인트 주소 변경
    "users" => "/api/members"

--- src/main/java/org/paz/community/member/controller/MemberController.java
+++ src/main/java/org/paz/community/member/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/members")
 public class MemberController {
     private final MemberServiceImpl commonMemberService;
 
