0db92c9b44a5c61b1468c5ff082fd8aec43cbd51
TopazKang <lasksla@gmail.com>
2024-10-25 17:40:31 +0900

    fix: Comment 레포지토리 JPA 쿼리 메서드 수정
    Soft Delete 반영을 위해 전체 댓글 데이터 조회시 PostEntity를 기준으로 deleted_at 칼럼이 null인 레코드만 조회해도로록 수정

--- src/main/java/org/paz/community/comment/repository/CommentRepository.java
+++ src/main/java/org/paz/community/comment/repository/CommentRepository.java
@@ -1,10 +1,11 @@
 package org.paz.community.comment.repository;
 
 import org.paz.community.comment.entity.CommentEntity;
+import org.paz.community.post.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-    List<CommentEntity> findByPostId(Long postId);
+    List<CommentEntity> findByPostEntityAndDeletedAtIsNull(PostEntity postEntity);
 }
