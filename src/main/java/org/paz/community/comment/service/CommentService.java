b79c13bc5ea41316cda1de53c76b71efd016a943
TopazKang <lasksla@gmail.com>
2025-01-26 20:57:13 +0900

    feature: comment - add isOwner in return dto

--- src/main/java/org/paz/community/comment/service/CommentService.java
+++ src/main/java/org/paz/community/comment/service/CommentService.java
@@ -7,7 +7,7 @@ public interface CommentService {
     void createComment(Long postId, String comment);
 
     // 단일 댓글 조회
-    ReadOneCommentResponseDto readOneComment(Long commentId);
+    ReadOneCommentResponseDto readOneComment(Long commentId, Long userId);
 
     //댓글 수정
     void modifyComment(Long commentId, String comment);
